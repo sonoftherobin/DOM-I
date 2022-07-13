2ca0c8ec4f9672d2f9f0f2cebab52d34fc512b2c
Aaron Robinson <aaronrobinson@Aarons-MacBook-Pro.local>
2022-07-13 10:01:54 -0600

    assignment complete

--- src/index.js
+++ src/index.js
@@ -39,7 +39,24 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+console.log('project wired!');
+//header
+const links = document.querySelectorAll("nav a");
+links.forEach(a=>{
+  a.className = "italic"
+})
+console.log(links);
+links[0].textContent = siteContent["nav"]["nav-item-1"];
+links[1].textContent = siteContent["nav"]["nav-item-2"];
+links[2].textContent = siteContent["nav"]["nav-item-3"];
+links[3].textContent = siteContent["nav"]["nav-item-4"];
+links[4].textContent = siteContent["nav"]["nav-item-5"];
+links[5].textContent = siteContent["nav"]["nav-item-6"];
+
+//logo
+const logo = document.querySelector("#logo-img");
+logo.src = siteContent["images"]["logo-img"];
+
 //h1
 const domHeader = document.querySelector("h1");
 domHeader.textContent = siteContent.cta.h1;
@@ -51,15 +68,50 @@ document.querySelector("button").textContent = siteContent.cta.button;
 //cta image
 const ctaImage = document.querySelector("#cta-img");
 ctaImage.src =siteContent["images"]["cta-img"];
-console.log(ctaImage);
 
-//top content
-const featuresHeader = document.querySelector("h4").setAttribute("className","features-h4");
-const featuresText = document.querySelector("p");
-// featuresHeader.textContent = siteContent["main-content"]["features-h4"];
+//section titles
+
+const titles = document.querySelectorAll(".main-content h4");
+const featuresTitle = titles[0];
+featuresTitle.textContent = siteContent["main-content"]["features-h4"];
+const aboutTitle = titles[1];
+aboutTitle.textContent = siteContent["main-content"]["about-h4"];
+const servicesTitle = titles[2];
+servicesTitle.textContent = siteContent["main-content"]["services-h4"];
+const productTitle = titles[3];
+productTitle.textContent = siteContent["main-content"]["product-h4"];
+const visionTitle = titles[4];
+visionTitle.textContent = siteContent["main-content"]["vision-h4"];
+
+//section text
+
+const text = document.querySelectorAll(".main-content p");
+const featuresText = text[0];
 featuresText.textContent = siteContent["main-content"]["features-content"];
-// document.querySelector(".top-content").appendChild(featuresHeader);
-document.querySelector(".top-content").appendChild(featuresText);
-console.log(featuresHeader);
+const aboutText = text[1];
+aboutText.textContent = siteContent["main-content"]["about-content"];
+const servicesText = text[2];
+servicesText.textContent = siteContent["main-content"]["services-content"];
+const productText = text[3];
+productText.textContent = siteContent["main-content"]["product-content"];
+const visionText = text[4];
+visionText.textContent = siteContent["main-content"]["vision-content"];
 
-const aboutHeader = document.querySelector("h4").textContent = siteContent["main-content"]["about-h4"];
+//main image
+const mainImage = document.querySelector(".middle-img").src =siteContent["images"]["accent-img"];
+
+//contact
+
+
+document.querySelector(".contact h4").textContent = siteContent["contact"]["contact-h4"];
+const info = document.querySelectorAll(".contact p")
+info[0].textContent = siteContent.contact.address;
+info[1].textContent = siteContent.contact.phone;
+info[2].textContent = siteContent.contact.email;
+
+//footer
+document.querySelector("footer a").textContent = siteContent.footer.copyright;
+document.querySelector("footer a").className = "bold"
+
+
+
